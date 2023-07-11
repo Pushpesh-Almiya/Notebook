334b726dc9f612687e8381e4281c389f3b74fe77
Pushpesh Almiya <almiyapushpesh55@gmail.com>
2023-07-11 23:06:50 +0530

    Reduce file structure and tailwind setup

--- backend/index.js
+++ backend/index.js
@@ -4,12 +4,11 @@ const app = express()
 const port = 8000;
 const authPath = require("./routes/auth")
 const notesPath = require("./routes/notes")
-
 //Available routes
-app.use(express.json());
+app.use(express.json())
+// app.use(express.urlencoded({extended: false}))
 app.use(authPath)
 app.use(notesPath)
-
 app.listen(port,()=>{
     console.log(`listening on http://localhost:${port}`)
 })
